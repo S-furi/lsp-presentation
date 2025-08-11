a0167c9f7f670d4204c81ba0ce6a252b9591771a
Stefano Furi <stefano.furi@jetbrains.com>
2025-08-11 16:25:53 +0200

    chore: pretty print LSP results

--- code/kt-lsp-example/src/main/kotlin/Main.kt
+++ code/kt-lsp-example/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import org.eclipse.lsp4j.Position
+import org.eclipse.lsp4j.Range
 import java.net.URI
 import java.nio.file.Paths
 import kotlin.system.exitProcess
@@ -11,36 +12,34 @@ val sourcesPath: URI = root.resolve("src/main/kotlin/")
 private fun testCompletions() {
     val completions = client.getCompletion(sourcesPath.resolve("CompleteMe.kt"), Position(2, 12)).get()
     println(" >>> Found ${completions.right?.items?.size ?: 0} completions:")
-    completions.apply { right?.items?.forEach { println("- ${it.label} (${it.kind}) - ${it.labelDetails}") } }
+    completions.apply { right?.items?.forEachIndexed { idx, it ->
+        println("""
+    - (${idx + 1}). ${it.label} (${it.kind}), type: ${it.labelDetails.description}
+        - found in ${it.labelDetails.detail}
+        - sorted by ${it.sortText}
+    """.trimIndent()) } }
 }
 
 private fun testHover() {
     val hoverResult = client.hover(sourcesPath.resolve("Main.kt"), Position(13, 12)).get() // Hover over `println`
-    println(" >>> Hover result:")
-    println(hoverResult?.contents?.right)
+    println(" >>> Hover result over `println`:")
+    println(hoverResult?.contents?.right?.value)
 }
 
 private fun testReferences() {
     val references = client.findReferences(sourcesPath.resolve("Main.kt"), Position(13, 12)).get()
         ?.filter { it.uri.startsWith("file") }
-    println(" >>> Found ${references?.size ?: 0} references:")
-    references?.forEach { println("- ${it.uri} ${it.range}") }
-}
-
-private fun testWorkspaceSymbol(query: String) {
-    val workspaceSymbols = client.workspaceSymbol(query).get()
-    println(" >>> Found the following workspace symbols for query '$query':")
-    println(workspaceSymbols?.right?.joinToString("\n") { "- ${it.name} [${it.kind}], (${it.location?.left})" } ?: "No results found.")
+    println(" >>> Found ${references?.size ?: 0} references for `println`:")
+    references?.forEach { println("- ${it.uri} ${it.range.prettyPrint()}") }
 }
 
 fun main() {
     listOf(
         { client.init(root.path).get() },
         { client.openDocument( sourcesPath.resolve("Main.kt")) },
-        { testCompletions() },
         { testHover() },
         { testReferences() },
-        { testWorkspaceSymbol("myWork") },
+        { testCompletions() },
         { client.closeDocument(sourcesPath.resolve("Main.kt")) },
         { client.shutdown().thenRun { client.exit() } },
     ).runAllDelayed(1000)
@@ -49,3 +48,5 @@ fun main() {
 
 fun List<() -> Any?>.runAllDelayed(delay: Long) =
     forEach { it(); Thread.sleep(delay) }
+
+fun Range.prettyPrint() = "From l:${start.line} c:${start.character} to l:${end.line} c:${end.character}"
